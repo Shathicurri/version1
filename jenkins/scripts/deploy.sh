724323e2b2706b5cbc94893b5d197500c2e3225b
shathicurri01 <shathiyasulthana@outlook.com>
2023-11-15 03:14:55 +0800

    test

---
+++ jenkins/scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+set -x
+docker run -d -p 80:80 --name nginx-app -v /home/version1:/usr/share/nginx/html nginx:alpine
+sleep 1
+set +x
+
+echo 'Now...'
+echo 'Visit http://localhost/ to see your html application in action.'
+
